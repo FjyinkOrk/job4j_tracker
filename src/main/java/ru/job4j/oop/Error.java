75cb75ba412f3a55a70d31cd7eabf9251a6dba6f
FlyingOrk <dudinmit@gmail.com>
2023-12-08 18:04:19 +0300

    1.7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,33 @@
+package ru.job4j.oop;
+
+public class Error {
+    boolean active;
+    int status;
+    String message;
+
+    public Error() {
+    }
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public void printInfo() {
+        System.out.println(active);
+        System.out.println(status);
+        System.out.println(message);
+    }
+
+    public static void main(String[] args) {
+        Error object1 = new Error();
+        Error object2 = new Error(true, 2, "Сообщение 2");
+        Error object3 = new Error(false, 3, "Сообщение 3");
+        Error object4 = new Error(true, 4, "Сообщение 4");
+        object1.printInfo();
+        object2.printInfo();
+        object3.printInfo();
+        object4.printInfo();
+    }
+}
