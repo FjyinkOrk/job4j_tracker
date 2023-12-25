2675a91e86863cee61281e01d92df4ad91b43a57
FlyingOrk <dudinmit@gmail.com>
2023-12-25 19:17:40 +0300

    5. Tracker - хранилище [#396]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -9,6 +11,19 @@ public class Tracker {
         item.setId(ids++);
         items[size++] = item;
         return item;
+    }
+
+    public Item[] findAll() {
+
+        Item[] rsl = new Item[size];
+        int size = 0;
+        for (int index = 0; index < items.length; index++) {
+            if (items[index] != null) {
+                rsl[size] = items[index];
+                size++;
+            }
+        }
+        return Arrays.copyOf(rsl, size);
     }
 
     public Item findById(int id) {
@@ -21,5 +36,19 @@ public class Tracker {
             }
         }
         return rsl;
+    }
+
+    public Item[] findByName(String key) {
+        Item[] rsl = new Item[size];
+        int size = 0;
+        for (int index = 0; index < items.length; index++) {
+            Item item = items[index];
+            if (item != null && item.getName().equals(key)) {
+
+                rsl[size] = items[index];
+                size++;
+            }
+        }
+        return Arrays.copyOf(rsl, size);
     }
 }
